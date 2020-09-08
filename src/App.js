692085accf3e1ff11a585f1217fd832d1be5e9b8
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-09-08 21:20:13 +0200

    Update project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
       <h1>Heyyy</h1>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
