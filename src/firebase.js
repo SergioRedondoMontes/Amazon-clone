c79d063ce1e5aeb5177c341b2367980e019548d5
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-09-08 21:02:47 +0200

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDa5pLplDe0VjMVVRpHsadYRhkCYtC4DuQ",
+  authDomain: "pruebajson-1e0c6.firebaseapp.com",
+  databaseURL: "https://pruebajson-1e0c6.firebaseio.com",
+  projectId: "pruebajson-1e0c6",
+  storageBucket: "pruebajson-1e0c6.appspot.com",
+  messagingSenderId: "589629753012",
+  appId: "1:589629753012:web:8dcc3c42ac1353b75779e1",
+};
