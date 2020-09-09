695dc19d70d8de1c1a68134ddb76cfeddb0ae1d8
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-09-09 11:45:02 +0200

    Home struct

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,27 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/AmazonDevices/2019/Post_AugArt/GW_Echo_PC_2x_V2._CB405879256_.jpg"
+          alt=""
+        />
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">{/* Product */}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
